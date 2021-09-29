38792157cc551bae0d20bd0c6bb746129a0b7183
kde <dkrivonosov@turnkey-lender.com>
2021-09-29 21:55:20 +0300

    sql task added

---
+++ SQL/CustomersOrders.sql
@@ -0,0 +1,12 @@
+﻿-- Customers and orders. A customer may have several orders, an order can belong to only 1 customer
+CREATE TABLE Customers (
+	Id int, -- PK
+	Name varchar(MAX)	
+)
+
+CREATE TABLE Orders (
+	Id int, -- PK
+	Customer_Id int -- FK to Customers
+)
+
+-- TODO: create a select statement to get IDs of customers that have no orders
